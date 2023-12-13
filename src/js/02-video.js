b3e26f4cc6551c430f029495b2fc59e255ace1ae
Piotrek <138370309+Shunii06@users.noreply.github.com>
2023-12-13 16:15:01 +0100

    final ver: changed vimeo.player to Vimeo. Now there s no uncought TypeError

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,10 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 document.addEventListener('DOMContentLoaded', () => {
   const iframe = document.getElementById('vimeo-player');
-  const player = new Vimeo.Player(iframe);
-
-  const saveCurrentTime = () => {
+  const player = new Vimeo(iframe);
+  const saveCurrentTime = throttle(() => {
     return player
       .getCurrentTime()
       .then(currentTime => {
@@ -10,72 +12,27 @@ document.addEventListener('DOMContentLoaded', () => {
           'videoplayer-current-time',
           JSON.stringify(currentTime)
         );
+        return { success: true, currentTime };
       })
       .catch(error => {
         console.error('An error occurred:', error);
+        return { success: false, error };
       });
-  };
+  }, 1000);
 
-  const throttledSaveCurrentTime = throttle(saveCurrentTime, 1000);
-
-  player.on('timeupdate', throttledSaveCurrentTime);
+  player.on('timeupdate', () => {
+    saveCurrentTime().then(({ success, currentTime, error }) => {});
+  });
 
   const setPlaybackFromStorage = () => {
     const storedTime = localStorage.getItem('videoplayer-current-time');
-    if (storedTime) {
-      const currentTime = JSON.parse(storedTime);
-      return player
-        .setCurrentTime(currentTime)
-        .then(() => {
-          // Dodaj obsługę powodzenia, jeśli to konieczne
-        })
-        .catch(error => {
-          console.error('An error occurred:', error);
-        });
-    }
-    return Promise.resolve();
+    return storedTime
+      ? player
+          .setCurrentTime(JSON.parse(storedTime))
+          .then(() => ({ success: true, currentTime: JSON.parse(storedTime) }))
+          .catch(error => ({ success: false, error }))
+      : Promise.resolve({ success: true });
   };
 
-  setPlaybackFromStorage();
-});
-//
-/*
-import Vimeo from '@vimeo/player';
-import throttle from 'lodash.throttle';
-
-const player = new Vimeo.Player(document.getElementById('vimeo-player'));
-
-const saveCurrentTime = throttle(() => {
-  return player
-    .getCurrentTime()
-    .then(currentTime => {
-      localStorage.setItem(
-        'videoplayer-current-time',
-        JSON.stringify(currentTime)
-      );
-      return { success: true, currentTime };
-    })
-    .catch(error => {
-      console.error('An error occurred:', error);
-      return { success: false, error };
-    });
-}, 1000);
-
-player.on('timeupdate', () => {
-  saveCurrentTime().then(({ success, currentTime, error }) => {});
-});
-
-const setPlaybackFromStorage = () => {
-  const storedTime = localStorage.getItem('videoplayer-current-time');
-  return storedTime
-    ? player
-        .setCurrentTime(JSON.parse(storedTime))
-        .then(() => ({ success: true, currentTime: JSON.parse(storedTime) }))
-        .catch(error => ({ success: false, error }))
-    : Promise.resolve({ success: true });
-};
-
-document.addEventListener('DOMContentLoaded', () => {
   setPlaybackFromStorage().then(({ success, currentTime, error }) => {});
 });
-*/
