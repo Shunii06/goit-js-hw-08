b13b88c54f3b0a1f16ac4d0aec8a4ab6c83f8bd7
Piotrek <138370309+Shunii06@users.noreply.github.com>
2023-12-13 16:56:30 +0100

    working task 3 feedback task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
+document.addEventListener('DOMContentLoaded', () => {
+  const feedbackForm = document.querySelector('.feedback-form');
+  const emailInput = feedbackForm.querySelector('input[name="email"]');
+  const messageTextarea = feedbackForm.querySelector(
+    'textarea[name="message"]'
+  );
 
+  const saveToLocalStorageThrottled = throttle(() => {
+    const formData = {
+      email: emailInput.value,
+      message: messageTextarea.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }, 500);
+
+  const loadFromLocalStorage = () => {
+    const storedData = localStorage.getItem('feedback-form-state');
+    if (storedData) {
+      const formData = JSON.parse(storedData);
+      emailInput.value = formData.email || '';
+      messageTextarea.value = formData.message || '';
+    }
+  };
+
+  feedbackForm.addEventListener('input', () => {
+    saveToLocalStorageThrottled();
+  });
+
+  window.addEventListener('load', () => {
+    loadFromLocalStorage();
+  });
+
+  feedbackForm.addEventListener('submit', event => {
+    event.preventDefault();
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageTextarea.value = '';
+
+    console.log('Form Data:', { email: '', message: '' });
+  });
+});
