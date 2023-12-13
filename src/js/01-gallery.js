f52007d337a206ab167ea40bee6cbe713bf8a350
Piotrek <138370309+Shunii06@users.noreply.github.com>
2023-12-13 13:06:36 +0100

    10th time try adding SLB

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+function createGalleryItem({ preview, original, description }) {
+  const listItem = document.createElement('li');
 
-console.log(galleryItems);
+  const galleryLink = document.createElement('a');
+  galleryLink.classList.add('gallery__item');
+  galleryLink.href = original;
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = preview;
+  image.alt = description;
+
+  galleryLink.appendChild(image);
+  listItem.appendChild(galleryLink);
+
+  return listItem;
+}
+galleryItems.forEach(item => {
+  const galleryItem = createGalleryItem(item);
+  galleryContainer.appendChild(galleryItem);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+});
